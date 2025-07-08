ad2d6f9285d10f79570ce996d534acf8457093b7
Rachel O <rkeonso@icloud.com>
2025-07-08 00:36:39 -0400

    Task1234

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,31 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null,11));
+  return res.status(300).json({message: "Book is not available"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(friends[isbn])
+  return res.status(300).json({message: "Book is not available"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  return res.status(300).json({message: "Book is not available"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title
+  return res.status(300).json({message: "Book is not available"});
 });
 
 //  Get book review
